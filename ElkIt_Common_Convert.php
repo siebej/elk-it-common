467c5b97f69fe108214073d6f463075b85d549cf
Siebe Jongebloed <siebe.jongebloed@gmail.com>
2016-07-24 10:53:29 +0200

    Divers

--- ElkIt_Common_Convert.php
+++ ElkIt_Common_Convert.php
@@ -5,6 +5,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once(ROOT_PATH.'lib\Encoding.php'); 
+use \ForceUTF8\Encoding;  // It's namespaced now.
 
 namespace ElkIt\Common;
 
--- ElkIt_Common_Server.php
+++ ElkIt_Common_Server.php
@@ -35,19 +35,42 @@ class Server {
         }
     }
     
-    public static function logErrorShellExec($command,$echoResult=FALSE){
-    $command        = $command.' 2>&1';
-//    echo_c('command', $command);
-    $result         = explode("\n",shell_exec($command));
-    if($echoResult){
-        print_r_c('result', $result);
-    }
-    $firstResult    = $result[0];
+    public static function getName() {
+        if(Check::isCommandLineInterface()){
+            return php_uname("n");
 
-    if (Xsl::contains($firstResult, 'not permitted') || Xsl::contains($firstResult,'command not found')){
-        trigger_error('command: '.$command.'. geeft fout: ' . $firstResult,E_USER_ERROR);
+        } elseif ($host = $_SERVER['HTTP_X_FORWARDED_HOST']) {
+            $elements = explode(',', $host);
+
+            $host = trim(end($elements));
+        } else {
+            if (!$host = $_SERVER['HTTP_HOST']) {
+                if (!$host = $_SERVER['SERVER_NAME']) {
+                    $host = !empty($_SERVER['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : '';
+                }
+            }
+        }
+
+        // Remove port number from host
+        $host = preg_replace('/:\d+$/', '', $host);
+
+        return trim($host);
     }
-    return $result;
-}
+
+    
+    public static function logErrorShellExec($command,$echoResult=FALSE){
+        $command        = $command.' 2>&1';
+    //    echo_c('command', $command);
+        $result         = explode("\n",shell_exec($command));
+        if($echoResult){
+            print_r_c('result', $result);
+        }
+        $firstResult    = $result[0];
+
+        if (Xsl::contains($firstResult, 'not permitted') || Xsl::contains($firstResult,'command not found')){
+            trigger_error('command: '.$command.'. geeft fout: ' . $firstResult,E_USER_ERROR);
+        }
+        return $result;
+    }
 
 }
